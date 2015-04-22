2a122c94d51c71c0f4dbbfdd7ab170a41c98d78e
Digeratus <digerat@gmail.com>
2015-04-21 21:06:55 -0400

    Started adding buttons. I'm using a vertical layout for now, but I   have to figure out how to put the buttons in the right places.

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ javacOptions ++= Seq("-source", "1.7", "-target", "1.7")
 
 android.Plugin.androidBuild
 
-name := "scaloidVideo"
+name := "ScaloidVideo"
 
 scalaVersion := "2.11.6"
 
--- src/scaloid/example/ScaloidVideoCapture.scala
+++ src/scaloid/example/ScaloidVideoCapture.scala
@@ -5,22 +5,38 @@ import android.graphics.Color
 
 class ScaloidVideoCapture extends SActivity {
 
-  onCreate {
-    contentView = new SVerticalLayout {
-      style {
-        case b: SButton => b.textColor(Color.RED).onClick(toast("Bang!"))
-        case t: STextView => t textSize 10.dip
-        case e: SEditText => e.backgroundColor(Color.YELLOW).textColor(Color.BLACK)
-      }
-      STextView("I am 10 dip tall... whatever that means")
-      STextView("Me too")
-      STextView("I am 15 dip tall") textSize 15.dip // overriding
-      this += new SLinearLayout {
-        STextView("Button: ")
-        SButton(R.string.red)
-      }.wrap
-      SEditText("ScaloidVideo is Yellow!").fill
+//  onCreate {
+//    contentView = new SVerticalLayout {
+//      style {
+//        case b: SButton => b.textColor(Color.RED).onClick(toast("Bang!"))
+//        case t: STextView => t textSize 10.dip
+//        case e: SEditText => e.backgroundColor(Color.YELLOW).textColor(Color.BLACK)
+//      }
+//      STextView("I am 10 dip tall... whatever that means")
+//      STextView("Me too")
+//      STextView("I am 15 dip tall") textSize 15.dip // overriding
+//      this += new SLinearLayout {
+//        STextView("Button: ")
+//        SButton(R.string.red)
+//      }.wrap
+//      SEditText("ScaloidVideo is Yellow!").fill
+//    } padding 20.dip
+//  }
+  onCreate { 
+    contentView = new SVerticalLayout { 
+      new SFrameLayout()
+      SImageButton().onClick(startRecording).imageResource(0x7f020005)
+      SButton("Yeah", toast("Yeah"))
+      SImageButton().onClick(stopRecording).imageResource(0x7f020003)
     } padding 20.dip
+  }
+  
+  def startRecording(): Unit = { 
+    toast("Recording!")
+  }
+  
+  def stopRecording(): Unit = { 
+    toast("Stopped!")
   }
 
 }
