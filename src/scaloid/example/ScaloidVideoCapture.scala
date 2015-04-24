ee192a986ad0a107a2ca5d8e02c8d35ea1b88055
Digeratus <digerat@gmail.com>
2015-04-23 21:11:30 -0400

    Forced layout into landscape and made the buttons toggle appropiately.

--- src/scaloid/example/ScaloidVideoCapture.scala
+++ src/scaloid/example/ScaloidVideoCapture.scala
@@ -1,11 +1,11 @@
 package scaloid.example
 
 import org.scaloid.common._
-import android.content.Intent
 import android.hardware.Camera
 import android.media.CamcorderProfile
 import android.media.MediaRecorder
 import android.view.Gravity
+import android.view.View
 
 class ScaloidVideoCapture extends SActivity {
 
@@ -27,21 +27,40 @@ class ScaloidVideoCapture extends SActivity {
 //    } padding 20.dip
 //  }
   var recording: Boolean = false;
+  
+  var startRecordingButton: SImageButton = null; 
+  var stopRecordingButton:  SImageButton = null; 
   onCreate { 
     val camera = Camera.open
     contentView =  new SFrameLayout {
-        SImageButton().onClick(startRecording).imageResource(0x7f020005).Gravity(Gravity.TOP)// .alignParentTop
         this += new CameraPreview(camera)
-        SImageButton().onClick(stopRecording).imageResource(0x7f020003).Gravity(Gravity.BOTTOM) //.alignParentBottom
+        startRecordingButton = SImageButton()
+          startRecordingButton.onClick(startRecording).imageResource(0x7f020005).Gravity(Gravity.BOTTOM)
+          startRecordingButton.setEnabled(true)
+          startRecordingButton.setVisibility(View.VISIBLE)
+        stopRecordingButton = SImageButton()
+          stopRecordingButton.onClick(stopRecording).imageResource(0x7f020003).Gravity(Gravity.BOTTOM)
+          stopRecordingButton.setEnabled(false)
+          stopRecordingButton.setVisibility(View.INVISIBLE)
     }
   }
   
   def startRecording(): Unit = { 
     toast("Recording!")
+    toggleButtons(false)
   }
   
   def stopRecording(): Unit = { 
     toast("Stopped!")
+    toggleButtons(true)
   }
 
+  def toggleButtons(recording: Boolean): Boolean = { 
+    val recordingStateNow = !recording
+    stopRecordingButton.setVisibility(if (recordingStateNow) View.VISIBLE else View.INVISIBLE)
+    stopRecordingButton.setEnabled(recordingStateNow)
+    startRecordingButton.setVisibility(if (recordingStateNow) View.INVISIBLE else View.VISIBLE)
+    startRecordingButton.setEnabled(!recordingStateNow)
+    recordingStateNow
+  }
 }
